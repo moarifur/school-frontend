3576e743d9b58fdf7d0f59ec6aaf4f3165bfd969
arif <moarifur@gmail.com>
2025-05-15 23:48:03 +0600

    Fixing issues of Tailwind, not working

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,7 @@ import {Button} from "@/components/ui/button";
 const Page = () => {
     return (
         <div>
-            <h1 className='bg-blue-500'>Yo</h1>
-            <Button className={`bg-blue-500`}>
-                <span className={`text-2xl font-sarif`}>Click me</span>
-
-            </Button>
+            Yo
         </div>
     );
 };
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+// tailwind.config.mjs
+export default {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}", // If applicable
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
