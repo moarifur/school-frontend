f7df1842a87b7323487b64b3945ee9d4689eff40
arif <moarifur@gmail.com>
2025-05-15 22:42:22 +0600

    ShadCN integration done successfully with this project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
     return (
         <div>
-          Hello
+            <h1 className='bg-blue-500'>Yo</h1>
+            <Button className={`bg-blue-500`}>
+                <span className={`text-2xl font-sarif`}>Click me</span>
+
+            </Button>
         </div>
     );
 };
