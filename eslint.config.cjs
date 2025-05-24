886213b05569894f1bf5036f2c58a8711497cbe2
arif <moarifur@gmail.com>
2025-05-24 09:23:55 +0600

    Fixbugs:: TypeScript ESLint Parser Missing Error Fix

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  rules: {
+    // Add your custom rules here
+  }
+};
